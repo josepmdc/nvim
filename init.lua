4f5f505b4329951972595bbe871b25a48aae039b
Josep Maria Domingo Catafal <josep.domingo@prima.it>
2026-02-23 17:18:06 +0100

    load plugins only when not running as root

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
-require("plugins")
 require("mappings")
 require("options")
 require("autocmds")
+-- Only load plugins when not runing as root
+if (vim.fn.exists('$SUDO_USER') == 0) then
+    require('plugins')
+end
